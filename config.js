f4f16ebc4c035ff0d718a67450b68537e9b27a6b
rudhra-prh <89623675+rudhra-prh@users.noreply.github.com>
2023-06-12 03:06:16 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -2,21 +2,21 @@ const fs = require('fs-extra')
 if (fs.existsSync('config.env')) require('dotenv').config({ path: __dirname+'/config.env' })
 
 
-//═══════[Required Variables]════════\\
+//═══════[Configuration Variables]════════\\
 global.owner = process.env.OWNER_NUMBER.split(",")
 global.mongodb = process.env.MONGODB_URI || "Enter-MongoURI-HERE"
 global.port= process.env.PORT || 5000
-global.email = 'sam@secktor.live'
-global.github = 'https://github.com/SamPandey001/Secktor-Md'
-global.location = 'Sultanpur IN'
-global.gurl = 'https://instagram.com/' // add your username
-global.sudo = process.env.SUDO || '919628516236'
-global.devs = '919628516236';
-global.website = 'https://github.com/SamPandey001/Secktor-Md' //wa.me/+91000000000000
-global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://raw.githubusercontent.com/SecktorBot/Brandimages/main/logos/SocialLogo%201.png'
+global.email = 'rudhraprh@gmail.com'
+global.github = 'https://github.com/princerudh/rudhra-md'
+global.location = 'Kerala IN'
+global.gurl = 'https://instagram.com/princerudh'
+global.sudo = process.env.SUDO || '919895809960'
+global.devs = '919895809960';
+global.website = 'https://github.com/princerudh/rudhra-md'
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://raw.githubusercontent.com/rudhra-prh/media/main/image/rudhra1.jpg'
 module.exports = {
-  botname: process.env.BOT_NAME || '𝐒𝐞𝐜𝐤𝐭𝐨𝐫 𝐁𝐨𝐭𝐭𝐨',
-  ownername:process.env.OWNER_NAME || 'SamPandey001',
+  botname: process.env.BOT_NAME || 'Rudhra',
+  ownername:process.env.OWNER_NAME || 'Ƥ ʀ ɪ ɴ ᴄ ᴇ  Ʀ ᴜ ᴅ ʜ',
   sessionName: process.env.SESSION_ID || 'PUT-HERE',
   author: process.env.PACK_INFO.split(";")[0] || 'author', 
   auto_read_status : process.env.AUTO_READ_STATUS || 'false',
